5f8b8fff3665f1c9d1fa99ab7f5f053660f3bf10
Anandhakumar <anandhaanavamani@gmail.com>
2026-08-10 10:59:30 +0530

    loops

---
+++ Python/condtional.py
@@ -0,0 +1,10 @@
+# if
+# if else
+# elif
+age = 20
+if age>=18: 
+    print("He is Eligible to vote")
+elif age<=13:
+    print("He is Teenager")
+else:
+    print("He is not eligible")
---
+++ Python/loops.py
@@ -0,0 +1,34 @@
+# while
+a=1
+while(a<10):
+    print("Hello Welcome")
+    a+=1
+
+# range
+# range(5)
+# 0 to 4
+
+# for x in range(12,0,-3):
+#     print(x)
+
+list = [10,20,40,60,80]
+
+names = "Arun"
+
+for i in names:
+    print(i)
+
+se = {10,20,40,60,80}
+
+# for i in se:
+#     print(i)
+
+
+# for i,v in enumerate(se):
+#     print(i,v)
+
+# for i in range(1000,10000):
+#     print(i)
+
+import random
+print(random.randint(999,9999))
