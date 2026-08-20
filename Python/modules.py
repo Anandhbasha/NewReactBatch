026e78880c4030fa99b5417da246619892506ccc
Anandhakumar <anandhaanavamani@gmail.com>
2026-08-20 10:36:37 +0530

    packs

---
+++ Python/modules.py
@@ -0,0 +1,8 @@
+def greet():
+    print("Hello Welcome")
+
+
+def add():
+    print("This is Additon")
+
+name = "Kausi"
---
+++ Python/packs.py
@@ -0,0 +1,47 @@
+# from modules import *
+
+
+# add()
+# greet()
+
+# print(name)
+
+# import math
+
+# print(math.sqrt(16))
+# print(int(math.pow(2,3)))
+
+
+# import random
+
+# # print(random.random())
+# # print(random.randint(1,10))
+# # print(random.randint(1000,9999))
+
+# list = ["arun","bala","Ravi","Ragul"]
+# print(random.choice(list))
+
+
+# json
+import json
+person = {
+    "name":"Aravind",
+    "age":20
+}
+
+print(type(person))
+
+Cjson = json.dumps(person)
+print(Cjson)
+print(type(Cjson))
+
+
+import time
+
+print("Start")
+time.sleep(3)
+print("End")
+
+# re
+# os
+# collection
