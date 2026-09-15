f3d3827afc5bec68406f68631433bb54eef40e56
Anandhakumar <anandhaanavamani@gmail.com>
2026-09-15 10:50:30 +0530

    urls

--- Python/Django/Inventory/views.py
+++ Python/Django/Inventory/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,"index.html")
+    context = {
+        "name":"Anandh"
+    }
+    return render(request,"index.html",context)
