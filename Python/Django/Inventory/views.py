4d93d812f17f775fe1450d47ba6add30e5e640c4
Anandhakumar <anandhaanavamani@gmail.com>
2026-09-16 10:58:19 +0530

    create

--- Python/Django/Inventory/views.py
+++ Python/Django/Inventory/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from.models import Product
 
 def home(request):
-    context = {
-        "name":"Anandh"
-    }
-    return render(request,"index.html",context)
+    Products = Product.objects.all()
+    return render(request,"index.html",{"Products":Products})
+
+def addProd(request):
+    if request.method=="POST":
+        name=request.POST["name"]
+        price=request.POST["price"]
+        qty=request.POST["qty"]
+        Product.objects.create(name=name,price=price,qty=qty)
+        return redirect("/inventory/home/")
+    return render(request,"addproduct.html")
